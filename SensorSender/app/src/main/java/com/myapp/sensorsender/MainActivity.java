8c8eb60c3c30f4743431bbf103390107d5b4da39
potok01 <apotok0001@gmail.com>
2023-04-04 18:10:44 -0400

    Handled broken pipe error from closing connection from client side, and from closing the connection on the server side

--- SensorSender/app/src/main/java/com/myapp/sensorsender/MainActivity.java
+++ SensorSender/app/src/main/java/com/myapp/sensorsender/MainActivity.java
@@ -17,92 +17,17 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private class MySensorEventListener implements SensorEventListener {
-        byte[] accelDataBytes = new byte[3 * 4];
-        ByteBuffer accelBuffer = ByteBuffer.wrap(accelDataBytes).order(ByteOrder.LITTLE_ENDIAN);
-        byte[] gyroDataBytes = new byte[3 * 4];
-        byte[] allDataBytes = new byte[accelDataBytes.length + gyroDataBytes.length];
-        ByteBuffer gyroBuffer = ByteBuffer.wrap(gyroDataBytes).order(ByteOrder.LITTLE_ENDIAN);
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            // Send the sensor data to the server
-            try {
-                // Convert the sensor data to byte arrays
-                for (int i = 0; i < event.values.length; i++) {
-                    if (!isAccelerometerDataAvailable && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                        accelBuffer.putFloat(i * 4, event.values[i]);
-                        if (i == event.values.length - 1) {
-                            isAccelerometerDataAvailable = true;
-                        }
-                    } else if (!isGyroscopeDataAvailable && event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                        gyroBuffer.putFloat(i * 4, event.values[i]);
-                        if (i == event.values.length - 1) {
-                            isGyroscopeDataAvailable = true;
-                        }
-                    }
-                }
-                if (isAccelerometerDataAvailable && isGyroscopeDataAvailable) {
-                    // Combine the byte arrays for accelerometer and gyroscope data
-                    System.arraycopy(accelDataBytes, 0, allDataBytes, 0, accelDataBytes.length);
-                    System.arraycopy(gyroDataBytes, 0, allDataBytes, accelDataBytes.length, gyroDataBytes.length);
-
-                    isAccelerometerDataAvailable = false;
-                    isGyroscopeDataAvailable = false;
-
-                    // Use the same ByteBuffer objects to read the float values
-                    ByteBuffer buffer = ByteBuffer.wrap(allDataBytes).order(ByteOrder.LITTLE_ENDIAN);
-
-                    for (int i = 0; i < 6; i++) {
-                        Log.d("Float", String.valueOf(buffer.getFloat()));
-                    }
-
-                    // Send the data to the server on a separate thread
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                // Write the data to the output stream
-                                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                                dataOutputStream.write(allDataBytes);
-                                dataOutputStream.flush();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // Do nothing
-        }
-    }
-    private Socket socket;
-    private OutputStream outputStream;
-    private Button connectButton;
-    private Button disconnectButton;
-    private Button startSendingButton;
-    private Button stopSendingButton;
-    private boolean isSendingData = false;
-    private SensorManager sensorManager;
-    private SensorEventListener sensorListener;
-    private boolean isAccelerometerDataAvailable = false;
-    private boolean isGyroscopeDataAvailable = false;
-    private static final int SENSOR_DELAY = SensorManager.SENSOR_DELAY_GAME;
-    private static final int SENSOR_TYPE_ACCELEROMETER = Sensor.TYPE_ACCELEROMETER;
-    private static final int SENSOR_TYPE_GYROSCOPE = Sensor.TYPE_GYROSCOPE;
+    public Button connectButton;
+    public Button disconnectButton;
+    public Button startSendingButton;
+    public Button stopSendingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -113,132 +38,34 @@ public class MainActivity extends AppCompatActivity {
         startSendingButton = findViewById(R.id.start_sending_button);
         stopSendingButton = findViewById(R.id.stop_sending_button);
 
+        SensorListener sensorListener = new SensorListener(MainActivity.this);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                connectToServer();
+                sensorListener.connectToServer();
             }
         });
 
         disconnectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                disconnectFromServer();
+                sensorListener.disconnectFromServer();
             }
         });
 
         startSendingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startSendingSensorData();
+                sensorListener.startSendingSensorData();
             }
         });
 
         stopSendingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopSendingSensorData();
+                sensorListener.stopSendingSensorData();
             }
         });
     }
-    private void connectToServer() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    socket = new Socket();
-                    socket.connect(new InetSocketAddress("10.0.0.157", 1234), 5000);
-                    outputStream = socket.getOutputStream();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Connected to server", Toast.LENGTH_SHORT).show();
-
-                            // Enable all buttons except the "Connect" button
-                            disconnectButton.setEnabled(true);
-                            startSendingButton.setEnabled(true);
-                            stopSendingButton.setEnabled(true);
-                            connectButton.setEnabled(false);
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Could not connect to server", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            }
-        }).start();
-
-    }
-    private void disconnectFromServer() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    socket.close();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Disconnected from server", Toast.LENGTH_SHORT).show();
-
-                            // Disable all buttons except the "Connect" button
-                            disconnectButton.setEnabled(false);
-                            startSendingButton.setEnabled(false);
-                            stopSendingButton.setEnabled(false);
-                            connectButton.setEnabled(true);
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-    private void startSendingSensorData() {
-        // Check if we're already sending data
-        if (isSendingData) {
-            return;
-        }
-
-        // Create a new thread to run the sensor data sending code
-        Thread sensorThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // Create the sensor manager and sensor listener
-                sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                sensorListener = new MySensorEventListener();
-
-                // Register the sensor listener for accelerometer and gyroscope sensors
-                Sensor accelerometerSensor = sensorManager.getDefaultSensor(SENSOR_TYPE_ACCELEROMETER);
-                sensorManager.registerListener(sensorListener, accelerometerSensor, SENSOR_DELAY);
-
-                Sensor gyroscopeSensor = sensorManager.getDefaultSensor(SENSOR_TYPE_GYROSCOPE);
-                sensorManager.registerListener(sensorListener, gyroscopeSensor, SENSOR_DELAY);
-
-                // Set the isSendingData flag to true
-                isSendingData = true;
-            }
-        });
-
-        // Start the thread
-        sensorThread.start();
-    }
-    private void stopSendingSensorData() {
-        // Check if we're currently sending data
-        if (!isSendingData) {
-            return;
-        }
-
-        // Unregister the sensor listener
-        sensorManager.unregisterListener(sensorListener);
-
-        // Set the flag indicating that we're not sending data
-        isSendingData = false;
-    }
-
-
 }
+
---
+++ SensorSender/app/src/main/java/com/myapp/sensorsender/SensorListener.java
@@ -0,0 +1,236 @@
+package com.myapp.sensorsender;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class SensorListener implements SensorEventListener {
+    private MainActivity mainActivity;
+
+    public SensorListener(MainActivity activity) {
+        this.mainActivity = activity;
+    }
+
+    private Socket socket;
+    private OutputStream outputStream;
+    private boolean isSendingData = false;
+    private SensorManager sensorManager;
+    private SensorEventListener sensorListener = this;
+    private boolean isAccelerometerDataAvailable = false;
+    private boolean isGyroscopeDataAvailable = false;
+    private static final int SENSOR_DELAY = SensorManager.SENSOR_DELAY_GAME;
+    private static final int SENSOR_TYPE_ACCELEROMETER = Sensor.TYPE_ACCELEROMETER;
+    private static final int SENSOR_TYPE_GYROSCOPE = Sensor.TYPE_GYROSCOPE;
+    byte[] accelDataBytes = new byte[3 * 4];
+    ByteBuffer accelBuffer = ByteBuffer.wrap(accelDataBytes).order(ByteOrder.LITTLE_ENDIAN);
+    byte[] gyroDataBytes = new byte[3 * 4];
+    byte[] allDataBytes = new byte[accelDataBytes.length + gyroDataBytes.length];
+    ByteBuffer gyroBuffer = ByteBuffer.wrap(gyroDataBytes).order(ByteOrder.LITTLE_ENDIAN);
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Send the sensor data to the server
+        try {
+            // Convert the sensor data to byte arrays
+            for (int i = 0; i < event.values.length; i++) {
+                if (!isAccelerometerDataAvailable && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    accelBuffer.putFloat(i * 4, event.values[i]);
+                    if (i == event.values.length - 1) {
+                        isAccelerometerDataAvailable = true;
+                    }
+                } else if (!isGyroscopeDataAvailable && event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                    gyroBuffer.putFloat(i * 4, event.values[i]);
+                    if (i == event.values.length - 1) {
+                        isGyroscopeDataAvailable = true;
+                    }
+                }
+            }
+            if (isAccelerometerDataAvailable && isGyroscopeDataAvailable) {
+                // Combine the byte arrays for accelerometer and gyroscope data
+                System.arraycopy(accelDataBytes, 0, allDataBytes, 0, accelDataBytes.length);
+                System.arraycopy(gyroDataBytes, 0, allDataBytes, accelDataBytes.length, gyroDataBytes.length);
+
+                isAccelerometerDataAvailable = false;
+                isGyroscopeDataAvailable = false;
+
+                // Use the same ByteBuffer objects to read the float values
+                ByteBuffer buffer = ByteBuffer.wrap(allDataBytes).order(ByteOrder.LITTLE_ENDIAN);
+
+                for (int i = 0; i < 6; i++) {
+                    Log.d("Float", String.valueOf(buffer.getFloat()));
+                }
+
+                // Send the data to the server on a separate thread
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // Check if the socket is still connected
+                            if (!socket.isConnected()) {
+                                Log.e("Socket", "Connection closed");
+                                return;
+                            }
+                            // Write the data to the output stream
+                            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                            dataOutputStream.write(allDataBytes);
+                            dataOutputStream.flush();
+                        } catch (SocketException e) {
+                            // Handle the broken pipe error
+                            Log.e("Socket", "Remote server closed");
+                            disconnectFromServer();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connectToServer() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket();
+                    socket.connect(new InetSocketAddress("10.0.0.157", 1234), 5000);
+                    outputStream = socket.getOutputStream();
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(mainActivity, "Connected to server", Toast.LENGTH_SHORT).show();
+
+                            // Enable all buttons except the "Connect" button
+                            mainActivity.connectButton.setEnabled(false);
+                            mainActivity.disconnectButton.setEnabled(true);
+                            mainActivity.startSendingButton.setEnabled(true);
+                            mainActivity.stopSendingButton.setEnabled(false);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(mainActivity, "Could not connect to server", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+
+    }
+
+    public void disconnectFromServer() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket.close();
+                    stopSendingSensorData();
+                    mainActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(mainActivity, "Disconnected from server", Toast.LENGTH_SHORT).show();
+
+                            // Disable all buttons except the "Connect" button
+                            mainActivity.connectButton.setEnabled(true);
+                            mainActivity.disconnectButton.setEnabled(false);
+                            mainActivity.startSendingButton.setEnabled(false);
+                            mainActivity.stopSendingButton.setEnabled(false);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void startSendingSensorData() {
+        // Check if we're already sending data
+        if (isSendingData) {
+            return;
+        }
+
+        // Create a new thread to run the sensor data sending code
+        Thread sensorThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Create the sensor manager and sensor listener
+                sensorManager = (SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE);
+
+                // Register the sensor listener for accelerometer and gyroscope sensors
+                Sensor accelerometerSensor = sensorManager.getDefaultSensor(SENSOR_TYPE_ACCELEROMETER);
+                sensorManager.registerListener(sensorListener, accelerometerSensor, SENSOR_DELAY);
+
+                Sensor gyroscopeSensor = sensorManager.getDefaultSensor(SENSOR_TYPE_GYROSCOPE);
+                sensorManager.registerListener(sensorListener, gyroscopeSensor, SENSOR_DELAY);
+
+                // Set the isSendingData flag to true
+                isSendingData = true;
+            }
+        });
+
+        // Start the thread
+        sensorThread.start();
+
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mainActivity, "Started sending data", Toast.LENGTH_SHORT).show();
+
+                // Enable all buttons except the "Connect" button
+                mainActivity.connectButton.setEnabled(false);
+                mainActivity.disconnectButton.setEnabled(true);
+                mainActivity.startSendingButton.setEnabled(false);
+                mainActivity.stopSendingButton.setEnabled(true);
+            }
+        });
+    }
+
+    public void stopSendingSensorData() {
+        // Check if we're currently sending data
+        if (!isSendingData) {
+            return;
+        }
+
+        // Unregister the sensor listener
+        sensorManager.unregisterListener(sensorListener);
+
+        // Set the flag indicating that we're not sending data
+        isSendingData = false;
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mainActivity, "Stopped sending data", Toast.LENGTH_SHORT).show();
+
+                // Enable all buttons except the "Connect" button
+                mainActivity.connectButton.setEnabled(false);
+                mainActivity.disconnectButton.setEnabled(true);
+                mainActivity.startSendingButton.setEnabled(true);
+                mainActivity.stopSendingButton.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
+    }
+}
